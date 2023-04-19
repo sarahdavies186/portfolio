66af632f9d43512491e1f3e000a5c6fcd41600e5
Sarah <sarahdavies186@gmail.com>
2023-04-19 12:15:52 +0100

    moved hero section to seperate component

---
+++ components/HeroSection.js
@@ -0,0 +1,11 @@
+export default function HeroSection() {
+  return (
+    <div className="flex flex-col pt-40 pl-10">
+      <h1 className="text-black font-tangerine text-8xl py-2">Sarah Davies</h1>
+      <h2 className="text-black font-syne-medium text-4xl pb-10">Software developer and designer
+      <br/>
+      based in the UK</h2>
+      <a className="text-black font-sportinggrotesqueregular text-lg pt-1">Scroll down ↓</a>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import HeroSection from '../components/HeroSection';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-between p-24 bg-nude">
-    <div className="flex flex-col pt-40 pl-10">
-      <h1 className="text-black font-tangerine text-8xl py-2">Sarah Davies</h1>
-      <h2 className="text-black font-syne-medium text-4xl pb-10">Software developer and designer
-      <br/>
-      based in the UK</h2>
-      <a className="text-black font-sportinggrotesqueregular text-lg pt-1">Scroll down ↓</a>
-    </div>
+    <HeroSection />
   </main>
   )
 }
