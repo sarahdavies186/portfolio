b036d6fb43f4dab5d8b65629ec31385b62bb55c2
Sarah <sarahdavies186@gmail.com>
2023-04-20 23:53:02 +0100

    added about me section and design projects

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import HeroSection from "../components/HeroSection";
 import Work from "../components/Work";
+import AboutMe from "../components/AboutMe";
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
         <section>
           <HeroSection />
         </section>
-        <section className="flex justify-center bg-white pt-20">
+        <section className="flex justify-center bg-white pt-32 pb-20">
+          <AboutMe />
+        </section>
+        <section className="flex justify-center bg-white pt-20 pb-32">
           <Work />
         </section>
       </main>
