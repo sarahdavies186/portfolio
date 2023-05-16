c429a228d98aaa98339a9e227a54ef209a0816a5
Sarah <sarahdavies186@gmail.com>
2023-05-16 22:47:21 +0100

    update to email address href

--- components/Contact.js
+++ components/Contact.js
@@ -7,9 +7,10 @@ export default function Contact() {
       <h3 className="font-syne-medium text-3xl pb-2 text-center">
        Contact me:
       </h3>
-      <p className="font-syne-medium text-center text-2xl">
+      <a className="font-syne-medium text-center text-2xl"
+      href="mailto:sarahdavies186@gmail.com" >
        sarahdavies186@gmail.com
-      </p>
+      </a>
       <p className="font-syne-medium text-center text-2xl">
       <a className="hover:text-pink ease-in-out duration-200" href="https://www.linkedin.com/in/sarahdavies1/">LinkedIn</a>
       </p>
